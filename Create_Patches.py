8ad9adad16270952161ef00e3e740a5f9e9f6ed7
FDFrost6 <nelson.pinheiro@outlook.de>
2025-05-20 17:43:33 +0200

    for loop eingebaut und projektstrucktur angepasst

--- Create_Patches.py
+++ Create_Patches.py
@@ -7,19 +7,15 @@ import tqdm
 import matplotlib.pyplot as plt
 from GeoPatch import TrainPatch
 
-Ortho = "data/230517_reflectance_ortho.tif"
-label1 = "data/Shapes.tif"
+patches_folder = "patches"
+os.makedirs(patches_folder, exist_ok=True)
 
-patch = TrainPatch(image= Ortho, label=label1, patch_size=256, stride=128, channel_first=True)
-
-patch.data_dimension()
-
-patch.patch_info()
-
-patch.save_Geotif(folder_name="tif", only_label=True)
-
-patch.visualize(folder_name='tif',patches_to_show=2,band_num=1,
-
-fig_size=(10, 20),dpi=96)
+for image in glob.glob("data/*.tif"):
+    Ortho = image
+    label1 = "data/label/shaperaster.tif"
+    patch = TrainPatch(image=Ortho, label=label1, patch_size=256, stride=128, channel_first=True)
+    patch.data_dimension()
+    patch.patch_info()
+    patch.save_Geotif(folder_name=patches_folder, only_label=True)
 
 
